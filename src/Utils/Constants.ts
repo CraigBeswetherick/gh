cee4d3eb9a3ba05116015128e7c2d3dee97c68c6
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-05 00:06:01 +0100

    updated constants

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -3,12 +3,15 @@ export const PAGE_ABOUT_URL: string = "/about";
 export const PAGE_ADMIN_URL: string = "/admin";
 export const PAGE_BLOG_URL: string = "/blog";
 export const PAGE_PORTFOLIO_URL: string = "/portfolio";
+export const PAGE_LOGIN_URL: string = "/login";
 
 export const PAGE_HOME_LABEL: string = "Home";
 export const PAGE_ABOUT_LABEL: string = "About";
 export const PAGE_ADMIN_LABEL: string = "Admin";
 export const PAGE_BLOG_LABEL: string = "Blog";
 export const PAGE_PORTFOLIO_LABEL: string = "Portfolio";
+export const PAGE_LOGIN_LABEL: string = "Login";
+export const PAGE_LOGOUT_LABEL: string = "Logout";
 
 export const TWITTER_URL: string = "https://twitter.com/grindheadgames";
 export const TWITTER_LABEL: string = "Grindhead Twitter";

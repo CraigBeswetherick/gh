3372671cf8637b570045c4bc935eb738fd5d5400
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-05 14:15:03 +0100

    updated images

--- src/Components/Header/HeaderView.tsx
+++ src/Components/Header/HeaderView.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import Navigation from "../Navigation";
 import "./header.scss";
 
+import Logo from "../../Images/logo.png";
+import Houses from "../../Images/houses.png";
+
 const HeaderView: React.FC = (props) => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <img src={Logo} alt="Logo" width="50%" height="auto" />;
+      <img src={Houses} alt="Houses" />;
+    </div>
+  );
 };
 
 export default HeaderView;
---
+++ src/Theme/AppBar.ts
@@ -0,0 +1,9 @@
+"use-strict";
+
+const AppBar = {
+  root: {
+    colorPrimary: "d7eefe",
+  },
+};
+
+export default AppBar;

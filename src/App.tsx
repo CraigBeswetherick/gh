eee5b623fac18d6e042ee4cddc74b9d2c38f1aa6
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-05 14:39:46 +0100

    added auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import Theme from "./Theme/Theme";
 import { Switch, Route, Router } from "react-router-dom";
@@ -11,24 +11,31 @@ import Routes from "./Utils/Routes";
 // Init Firebase
 import "./Utils/Firebase";
 
+// Auth
+export const AuthContext = React.createContext({});
+
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <MuiThemeProvider theme={Theme}>
-        <Router history={history}>
-          <Switch>
-            {Routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.main()}
-              />
-            ))}
-          </Switch>
-        </Router>
-      </MuiThemeProvider>
-    </div>
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      <div className="App">
+        <MuiThemeProvider theme={Theme}>
+          <Router history={history}>
+            <Switch>
+              {Routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main()}
+                />
+              ))}
+            </Switch>
+          </Router>
+        </MuiThemeProvider>
+      </div>
+    </AuthContext.Provider>
   );
 }
 

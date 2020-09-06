9d3f623bb409331ed340476df5d287c8f09de3b2
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-06 23:03:42 +0100

    updates

--- src/Pages/Login/LoginView.tsx
+++ src/Pages/Login/LoginView.tsx
@@ -1,16 +1,104 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import Navigation from "../../Components/Navigation";
 
-const LoginView: React.FC = (props) => {
+import { withStyles } from "@material-ui/styles";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+
+const styles = () => ({
+  "@global": {
+    body: {
+      backgroundColor: "#fff",
+    },
+  },
+  paper: {
+    marginTop: 100,
+    display: "flex",
+    padding: 20,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    backgroundColor: "#f50057",
+  },
+  form: {
+    marginTop: 1,
+  },
+  errorText: {
+    color: "#f50057",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+});
+
+interface LoginViewInterface {
+  handleEmailChange: Function;
+  handlePasswordChange: Function;
+  handleSubmit: Function;
+}
+
+const LoginView: React.FC = (props: LoginViewInterface) => {
+  const { classes } = this.props;
+
   return (
     <div>
       <Navigation />
       <Header />
       <div className="page">
-        <Typography>Login</Typography>
+        <Container component="main" maxWidth="xs">
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              onChange={props.handleEmailChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={props.handlePasswordChange}
+            />
+            {loginError && (
+              <Typography component="p" className={classes.errorText}>
+                Incorrect email or password.
+              </Typography>
+            )}
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={props.handleSubmit}
+            >
+              Sign In
+            </Button>
+          </Paper>
+        </Container>
       </div>
       <Footer />
     </div>
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import LoginView from "./LoginView";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { loginUser } from "../../Actions/index";
+import * as Constants from "../../Utils/Constants";
 
 const Login: React.FC = (props) => {
-  return <LoginView />;
+  const { loginError, isAuthenticated } = this.props;
+
+  const handleSubmit = () => {
+    const { dispatch } = props;
+    const { email: string, password: string } = this.state;
+    dispatch(loginUser(user, password));
+  };
+
+  const handleEmailChange = ({ target }) => {};
+
+  const handlePasswordChange = ({ target }) => {};
+
+  if (isAuthenticated) {
+    return <Redirect to={Constants.PAGE_ADMIN_URL} />;
+  } else {
+    return (
+      <LoginView
+        handleEmailChange={handleEmailChange}
+        handlePasswordChange={handlePasswordChange}
+        handleSubmit={handleSubmit}
+      />
+    );
+  }
 };
 
-export default Login;
+function mapStateToProps(state: any) {
+  return {
+    isLoggingIn: state.auth.isLoggingIn,
+    loginError: state.auth.loginError,
+    isAuthenticated: state.auth.isAuthenticated,
+  };
+}
+
+export default connect(mapStateToProps)(Login);

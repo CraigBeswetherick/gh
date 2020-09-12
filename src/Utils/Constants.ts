9272d03eb3c10d20e58fc7421ae6a2803c19afc1
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-12 16:26:45 +0100

    added link to register page from login

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -13,7 +13,8 @@ export const PAGE_ADMIN_LABEL: string = "Admin";
 export const PAGE_BLOG_LABEL: string = "Blog";
 export const PAGE_PORTFOLIO_LABEL: string = "Portfolio";
 export const PAGE_LOGIN_LABEL: string = "Login";
-export const PAGE_REGISTER_LABEL: string = "Register";
+export const PAGE_REGISTER_LABEL: string =
+  "Don't have an account? Register here.";
 export const PAGE_LOGOUT_LABEL: string = "Logout";
 
 export const TWITTER_URL: string = "https://twitter.com/grindheadgames";

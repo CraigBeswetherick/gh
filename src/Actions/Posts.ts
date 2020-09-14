ade3c4c9485cc86bf803d9480007cf1b1e26ffbd
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-14 16:39:29 +0100

    added post actions

---
+++ src/Actions/Posts.ts
@@ -0,0 +1,15 @@
+export const POST_CREATE_REQUEST: string = "POST_CREATE_REQUEST";
+export const POST_CREATE_SUCCESS: string = "POST_CREATE_SUCCESS";
+export const POST_CREATE_FAILURE: string = "POST_CREATE_FAILURE";
+
+export const POST_EDIT_REQUEST: string = "POST_EDIT_REQUEST";
+export const POST_EDIT_SUCCESS: string = "POST_EDIT_SUCCESS";
+export const POST_EDIT_FAILURE: string = "POST_EDIT_FAILURE";
+
+export const POST_DELETE_REQUEST: string = "POST_DELETE_REQUEST";
+export const POST_DELETE_SUCCESS: string = "POST_DELETE_SUCCESS";
+export const POST_DELETE_FAILURE: string = "POST_DELETE_FAILURE";
+
+export const POST_PUBLISH_REQUEST: string = "POST_PUBLISH_REQUEST";
+export const POST_PUBLISH_SUCCESS: string = "POST_PUBLISH_SUCCESS";
+export const POST_PUBLISH_FAILURE: string = "POST_PUBLISH_FAILURE";

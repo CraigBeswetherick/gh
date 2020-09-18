32d9c8abb4977a8e0336cb90a4b304852c107672
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-18 16:42:46 +0100

    upgrades

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,15 @@
-const createUser = () =>
-  functions.auth
-    .user()
-    .actions.auth.user()
-    .onCreate(async (user) => {
-      return admin.database().ref(`/users/${user.uid}`).update({
-        registeredDate: new Date().toISOString(),
-      });
-    });
+const functions = require("firebase-functions");
 
-const createPost = () =>
-  functions.auth.user().onCreate(async (post) => {
-    //create a ref to the posts on the user
-    //create a ref to the author on the post
+const createUser = functions.auth.user().onCreate((user) => {
+  return admin.database().ref(`/users/${user.uid}`).update({
+    registeredDate: new Date().toISOString(),
   });
+});
+
+const createPost = functions.auth.user().onCreate((post) => {
+  //create a ref to the posts on the user
+  //create a ref to the author on the post
+});
 
 exports.createUser = createUser;
 exports.createPost = createPost;

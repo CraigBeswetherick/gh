e3a542b9b70f28df9f36bf89c82ab216eb124654
craig beswetherick <craigbeswetherick@gmail.com>
2020-09-18 16:25:57 +0100

    updates

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,18 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require("firebase-functions");
+const createUser = () =>
+  functions.auth
+    .user()
+    .actions.auth.user()
+    .onCreate(async (user) => {
+      return admin.database().ref(`/users/${user.uid}`).update({
+        registeredDate: new Date().toISOString(),
+      });
+    });
 
-// The Firebase Admin SDK to access Cloud Firestore.
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-exports.createUser = functions.auth.user().onCreate(async (user) => {
-  return admin.database().ref(`/users/${user.uid}`).update({
-    registeredDate: new Date().toISOString(),
+const createPost = () =>
+  functions.auth.user().onCreate(async (post) => {
+    //create a ref to the posts on the user
+    //create a ref to the author on the post
   });
-});
+
+exports.createUser = createUser;
+exports.createPost = createPost;
